6b59e7e237a14504a011c3a67c46e22e4dcf6c6a
Henit Chobisa <chobisa.henit@gmail.com>
2023-07-11 09:50:15 +0000

    feat: Added Constants

---
+++ Packages/constants/constants.go
@@ -0,0 +1,12 @@
+package constants
+
+// Colors
+const (
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Cyan   = "\033[36m"
+	White  = "\033[37m"
+	Purple = "\033[35m"
+)
